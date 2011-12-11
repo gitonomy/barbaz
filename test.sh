7cece8e4e39414fc14001fe5fd2cfe8763caeb46
alice <alice@example.org>
2011-12-11 18:34:32 +0100

    Add a test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This is a test, and the content is going to be modified"
+echo "The aim is to:"
+echo " - Change the mode"
+echo " - Change the filename"
+echo " - Add some diff to the file"
+
