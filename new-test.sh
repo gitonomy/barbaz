5a519c47166f333e32091e166eb55f729106c964
alice <alice@example.org>
2011-12-11 19:22:35 +0100

    Remove a file

--- new-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "This is a test, and the content is going to be modified"
-echo "The aim is to:"
-echo " - Change the mode"
-echo " - Change the filename"
-echo " - Add some diff to the file"
-echo ""
-echo "It's done"
-
